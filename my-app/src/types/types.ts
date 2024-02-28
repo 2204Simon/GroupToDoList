ed2922364e6fc79017e9119c10c1765e7f73647e
kargmattis <115991382+kargmattis@users.noreply.github.com>
2024-02-28 10:36:08 +0100

    Add user and groupTodoList types

--- my-app/src/types/types.ts
+++ my-app/src/types/types.ts
@@ -12,3 +12,57 @@ export interface TodoListProps {
 export interface TodoListItemProps {
   todo: Todo;
 }
+
+[
+  {
+    user: [
+      { userId: "string", password: "string", email: "string" },
+      { userId: "string", password: "string", email: "string" },
+    ],
+
+    groupTodoList: [
+      {
+        groupListId: "string",
+        createdAt: "string",
+        updatedAt: "string",
+        title: "string",
+        description: "string",
+        deleted: "boolean",
+        rights: [
+          {
+            admins: [{ userId: "exampleUser", favorite: "boolean" }],
+          },
+          { editor: [{ userId: "exampleUser", favorite: "boolean" }] },
+          { reader: [{ userId: "exampleUser", favorite: "boolean" }] },
+        ],
+      },
+
+      {
+        todos: [
+          {
+            todoId: "string",
+          },
+        ],
+
+        todo: [
+          {
+            todoId: "string",
+            title: "string",
+            description: "string",
+            completed: "boolean",
+            deleted: "boolean",
+            deadline: "Date",
+            createdAt: "string",
+            updatedAt: "string",
+            label: {
+              labelId: "string",
+              name: "string",
+              color: "string",
+              userId: "string",
+            },
+          },
+        ],
+      },
+    ],
+  },
+];
