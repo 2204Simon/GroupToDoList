a2f29c81a341197c771748b469d8b47b93644074
2204Simon <sigl.simon@web.de>
2024-03-29 15:42:48 +0100

    Aktualisiere SingleToDoList-Komponente

--- my-app/src/components/SingleToDoList.tsx
+++ my-app/src/components/SingleToDoList.tsx
@@ -9,22 +9,22 @@ export default function SingleToDoList() {
       <div>SingleToDoList Nummer {id}</div>
       <button onClick={() => createTodo('testtodo', 'Test', 'Test', false)}>
         create LocalDatabases
-      </button>
+      </button> <br />
       <button onClick={async () => {
         const todos = await loadTodos('testtodo');
         console.log(todos);
       }}>
         load LocalDatabases
-      </button>
+      </button> <br />
       <button onClick={() => deleteTodo('testtodo', 'fea10ebb-f17d-4fb6-b58e-07f1d641995c')}>
         delete LocalDatabases
-      </button>
+      </button> <br />
       <button onClick={() => updateTodo('testtodo', 'eded3bbb-09a2-474b-8c0b-3c8f6ad8afff', {title: 'Updated', description: 'Updated', completed: true})}>
         update LocalDatabases
-      </button>
+      </button> <br />
       <button onClick={() => completeTodo('testtodo', 'cd0a6a6e-bd9d-4f14-a6ff-1593530d5bda', true)}>
         complete LocalDatabases
-      </button>
+      </button> 
     </div>
   )
 }
