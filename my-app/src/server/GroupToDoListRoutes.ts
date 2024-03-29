b9721c831d269a146e05f28b0da4d7347e016d80
2204Simon <sigl.simon@web.de>
2024-03-29 10:31:30 +0100

    Aktualisiere GroupToDoListRoutes.ts

--- my-app/src/server/GroupToDoListRoutes.ts
+++ my-app/src/server/GroupToDoListRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { couch } from './server'; // Importieren Sie die CouchDB-Instanz aus Ihrer Hauptserverdatei
+import { couch } from './server.ts' // Importieren Sie die CouchDB-Instanz aus Ihrer Hauptserverdatei
 import { v4 as uuidv4 } from 'uuid';
 import { validate as isUuid } from 'uuid';
 import { getUserIdFromToken } from './jwtMiddleware.ts';
@@ -15,7 +15,7 @@ GroupToDoListRoutes.post('/todolists', async (req, res) => {
     const { title } = req.body
     const { token } = req.cookies
     const id = uuidv4() // Generieren Sie eine eindeutige ID
-    const dbName = `db_${id}`
+    const dbName = `${id}`
 
     const dbs = await couch.listDatabases()
     if (!dbs.includes(testToken)) {
@@ -43,6 +43,9 @@ GroupToDoListRoutes.get('/todolists', async (req, res) => {
       return
     }
     const userId = getUserIdFromToken(req.cookies.token)
+     
+    
+
 
     if (!userId) {
       res.status(404).json({ error: 'User not found' })
@@ -55,7 +58,7 @@ GroupToDoListRoutes.get('/todolists', async (req, res) => {
       user.data.groupTodoLists
         .filter((todoList: { _id: any; role: any }) => isUuid(todoList._id))
         .map(async (todoList: { _id: any; role: any }) => {
-          const dbName = `db_${todoList._id}`
+          const dbName = `${todoList._id}`
           if (!dbs.includes(dbName)) {
             return null // Wenn die Datenbank nicht existiert, überspringen Sie sie
           }
@@ -65,6 +68,36 @@ GroupToDoListRoutes.get('/todolists', async (req, res) => {
         }),
     )
     res.json(todolists.filter((todoList: any) => todoList !== null))
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+GroupToDoListRoutes.put('/todolists/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const { title } = req.body
+    const dbName =  `${id}`
+
+    const dbs = await couch.listDatabases()
+    if (!dbs.includes(dbName)) {
+      res.status(404).json({ error: 'Datenbank nicht gefunden' })
+      return
+    }
+
+    // Finden Sie das Dokument mit der angegebenen id
+    const mangoQuery = { selector: { id: id } }
+    const { data: docs } = await couch.mango(dbName, mangoQuery)
+    if (docs.docs.length === 0) {
+      res.status(404).json({ error: 'Dokument nicht gefunden' })
+      return
+    }
+
+    const doc = docs.docs[0]
+    const updatedDoc = { ...doc, title } // Aktualisieren Sie den Titel
+    await couch.insert(dbName, updatedDoc) // Aktualisieren Sie das Dokument
+    res.json({ message: 'Todo-Liste erfolgreich aktualisiert' })
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Internal Server Error' })
